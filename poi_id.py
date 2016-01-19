13368e2455c4abbefb30f4086f1b3de0ae214274
shmuelnaaman <shmuel.naaman@yahoo.com>
2016-01-19 00:12:20 -0800

    some editing for the code

--- poi_id.py
+++ poi_id.py
@@ -11,9 +11,9 @@ from tester import test_classifier, dump_classifier_and_data
 # from sklearn import cross_validation
 
 '''
-Task 1: Select what features you'll use.
+Select what features 
 features_list is a list of strings, each of which is a feature name.
-The first feature must be "poi".
+The first feature must is  "poi".
 '''
 financial_features = ['salary', 'deferral_payments', 'total_payments',
                       'loan_advances', 'bonus', 'restricted_stock_deferred',
@@ -44,13 +44,13 @@ features_list = ['poi', 'salary', 'bonus', 'deferred_income', 'expenses',
                  'Norm_Email_T']
 
 
-# ## Load the dictionary containing the dataset
+# Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r"))
 
-# ## Task 2: Remove outliers
+# Remove outliers
 data_dict.pop('TOTAL', 0)
 
-# ## Scaling financial_features
+# Scaling financial_features
 
 for i_feat in financial_features:
     vec = []
@@ -67,11 +67,11 @@ for i_feat in financial_features:
             data_dict[i][i_feat] = ((data_dict[i][i_feat] - min(vec)) /
                                     float(max(vec) - min(vec)))
 '''
-Task 3: Create new feature(s)
+Create new feature(s)
 Store to my_dataset for easy export below.
 Extract features and labels from dataset for local testing
 '''
-# ## Scaling Email feature
+# Scaling Email feature
 
 for i in data_dict:
         A_F_S = data_dict[i]['shared_receipt_with_poi']
@@ -110,11 +110,8 @@ data = featureFormat(my_dataset, features_list, sort_keys=True,
 # The final features of the data base arranged as  labels and features
 labels, features = targetFeatureSplit(data)
 
-# ## Task 4: Try a varity of classifiers
-# ## Please name your classifier clf for easy export below.
-# ## Note that if you want to do PCA or other multi-stage operations,
-# ## you'll need to use Pipelines. For more info:
-# ## http://scikit-learn.org/stable/modules/pipeline.html
+# A varity of classifiers
+
 
 # AdaBoostClassifier using DecisionTreeClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -194,7 +191,7 @@ print "TP: {} TN: {} FP: {} FN: {}".format(confusion_matrix2[True, True],
 print "The precision of this classifier?", (confusion_matrix[True, True])/float(confusion_matrix[True, True] + confusion_matrix[False, True])
 print "The recall of this classifier?", (confusion_matrix[True, True])/(float(confusion_matrix[True, True] + confusion_matrix[True, False]))
 
-# Optional optimization using GridSearchCV
+# optimization using GridSearchCV
 '''
 from sklearn.grid_search import GridSearchCV
 
@@ -229,15 +226,13 @@ param_grid = {
 
 # #############################################################################
 
-# # Task 5: Tune your classifier to achieve better than .3 precision and recall
-# ## using our testing script.
+# # Tune your classifier to achieve better than  precision and recall
+
 # ## Because of the small size of the dataset, the script uses stratified
-# ## shuffle split cross validation. For more info:
-# ## http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+# ## shuffle split cross validation. 
 
 test_classifier(clf, my_dataset, features_list)
 
-# ## Dump your classifier, dataset, and features_list so
-# ## anyone can run/check your results.
+
 
 dump_classifier_and_data(clf, my_dataset, features_list)
